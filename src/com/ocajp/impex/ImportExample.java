17cb8eea306b7827efc329509a9600f1ba25cae5
Kachanau <dzianiskachanau@ibagroup.eu>
2021-09-20 10:43:26 +0300

    Initial commit

---
+++ src/com/ocajp/impex/ImportExample.java
@@ -0,0 +1,10 @@
+package com.ocajp.impex;
+
+import java.util.Random;
+
+public class ImportExample {
+    public static void main(String[] args) {
+        Random random = new Random();
+        System.out.println(random.nextInt(5));
+    }
+}
---
+++ src/com/ocajp/main/Main.java
@@ -0,0 +1,11 @@
+package com.ocajp.main;
+
+public class Main {
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("args-size = " + args.length);
+    }
+}
