f2686b0a30e7a405a58c26fe4ad4e7a532648c9c
Kachanau <dzianiskachanau@ibagroup.eu>
2021-09-20 11:18:07 +0300

    Add numbers

--- src/com/ocajp/hello/Hello.java
+++ src/com/ocajp/hello/Hello.java
@@ -2,6 +2,22 @@ package com.ocajp.hello;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World!\n");
+        helloWorld();
+        variables();
+    }
+
+    private static void helloWorld() {
+        System.out.println("Hello World!");
+    }
+
+    private static void variables() {
+        int my1stNumber = 35;
+        int my2ndNumber = my1stNumber * 2;
+        int my3rdNumber = 6;
+        int myTotal = my2ndNumber + my3rdNumber;
+        int myLastOne = myTotal - 1000;
+
+        System.out.println("myTotal = " + myTotal);
+        System.out.println("myLastOne = " + myLastOne);
     }
 }
